50e5d609336b29eed0325353291cbf3cd052b93b
klabit87 <keith.labit@gmail.com>
2017-03-28 20:04:56 -0500

    Adapt for LineageOS

---
+++ lineage.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/hero2ltevzw/full_hero2ltevzw.mk)
+
+PRODUCT_DEVICE := hero2ltevzw
+PRODUCT_NAME := lineage_hero2ltevzw
