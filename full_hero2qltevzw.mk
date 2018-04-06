cff342e3b40e0a891a35ed2aeee492811d00e0c3
klabit87 <keith.labit@gmail.com>
2018-04-06 18:57:24 -0500

    Go back to using Omni

--- full_hero2qltevzw.mk
+++ full_hero2qltevzw.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := hero2qltevzw
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
@@ -14,6 +14,6 @@ PRODUCT_PACKAGES += \
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hero2qltevzw
 PRODUCT_MODEL := SM-G935V
-PRODUCT_NAME := lineage_hero2qltevzw
+PRODUCT_NAME := omni_hero2qltevzw
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
--- omni_hero2qltevzw.mk
+++ omni_hero2qltevzw.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hero2qltevzw/full_hero2qltevzw.mk)
 
 PRODUCT_DEVICE := hero2qltevzw
-PRODUCT_NAME := lineage_hero2qltevzw
+PRODUCT_NAME := omni_hero2qltevzw
